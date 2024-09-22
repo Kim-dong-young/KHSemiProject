eb3ec553a53d0a643d938b7ccd0bdca2b464dc19
dydwns036 <dydwns036@gmail.com>
2024-09-22 11:02:28 +0900

    로그인 기능 수행중

---
+++ KHSemiProject/src/main/java/com/kh/member/controller/LoginController.java
@@ -0,0 +1,60 @@
+package com.kh.member.controller;
+
+import java.io.IOException;
+
+import com.kh.member.model.vo.Member;
+import com.kh.member.service.MemberService;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class loginController
+ */
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		String memberId = request.getParameter("memberId");
+		String memberPwd = request.getParameter("memberPwd");
+		
+		Member loginMember = new MemberService().loginMember(memberId, memberPwd);
+		
+		HttpSession session = request.getSession();
+		
+		if(loginMember == null) {
+			session.setAttribute("errorMsg", "로그인에 실패하였습니다.");
+			
+			request.getRequestDispatcher("templates/mainPage.jsp").forward(request, response);
+		} else {
+			session.setAttribute("loginMember", loginMember);
+			
+			request.getRequestDispatcher("templates/mainPage.jsp").forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
