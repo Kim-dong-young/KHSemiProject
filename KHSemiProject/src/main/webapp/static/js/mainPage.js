91ddcdf6386a80d860f3f6b542463b83f28d17ed
dydwns036 <dydwns036@gmail.com>
2024-10-01 19:36:38 +0900

    올바른 커밋

--- KHSemiProject/src/main/webapp/static/js/mainPage.js
+++ KHSemiProject/src/main/webapp/static/js/mainPage.js
@@ -1,14 +1,24 @@
 function initSwiper() {
-    const swiper = new Swiper('.swiper', {
-        // Optional parameters
-        slidesPerView: 6,
-        spaceBetween: 10,
-        loop: true,
+  const swiper = new Swiper('.swiper', {
+      // Optional parameters
+      slidesPerView: 6,
+      spaceBetween: 10,
+      loop: true,
 
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      }); 
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    }); 
+}
+
+function turn(activeIndex) {
+  // 모든 버튼을 선택
+  const buttons = document.querySelectorAll('.custom-btn');
+
+  // 모든 버튼을 순회하며 클릭된 버튼은 비활성화, 나머지 버튼은 활성화
+  buttons.forEach((button, index) => {
+      button.disabled = (index === activeIndex); // 클릭된 버튼만 비활성화
+  });
 }
