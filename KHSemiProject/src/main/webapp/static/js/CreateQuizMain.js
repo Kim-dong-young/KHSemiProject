38f666f7c1e98e0fff2b2b173ae35458c95d05bd
tkdwns3269 <tkdwns3269@naver.com>
2024-10-06 20:36:13 +0900

    js 태그 구현

--- KHSemiProject/src/main/webapp/static/js/CreateQuizMain.js
+++ KHSemiProject/src/main/webapp/static/js/CreateQuizMain.js
@@ -13,3 +13,35 @@ document.getElementById('thumbnail').addEventListener('change', function(event) 
     }
 });
 
+// 카테고리 선택 시 태그 입력 처리
+const categorySelect = document.getElementById('category');
+const tagInput = document.getElementById('tag');
+
+categorySelect.addEventListener('change', function() {
+    const selectedValue = categorySelect.value;
+
+    if (selectedValue === '8') { 
+        tagInput.disabled = false; 
+        tagInput.value = ''; 
+    } else {
+        tagInput.disabled = true; 
+        switch (selectedValue) {
+            case '1': tagInput.value = '유머'; break;
+            case '2': tagInput.value = '예술/문학'; break;
+            case '3': tagInput.value = '세계'; break;
+            case '4': tagInput.value = '역사'; break;
+            case '5': tagInput.value = '언어'; break;
+            case '6': tagInput.value = '과학/자연'; break;
+            case '7': tagInput.value = '스포츠'; break;
+            default: tagInput.value = ''; break;
+        }
+    }
+});
+
+
+window.addEventListener('load', function() {
+    const selectedValue = categorySelect.value;
+    if (selectedValue !== '8') { 
+        tagInput.disabled = true;
+    }
+});
