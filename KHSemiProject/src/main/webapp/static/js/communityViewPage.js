7fec2593b21f69e41c54e04b7351f4169817b8fc
Kim-dong-young <rlaehddud9502@naver.com>
2024-10-07 20:19:32 +0900

    추가 - 커뮤니티 글&댓글 신고기능 추가

--- KHSemiProject/src/main/webapp/static/js/communityViewPage.js
+++ KHSemiProject/src/main/webapp/static/js/communityViewPage.js
@@ -57,15 +57,52 @@ function submitComment(_this){
     }
 }
 
-function report(cno, mno){ // 피신고자 커뮤니티 번호, 멤버번호 
+function changeSubmitBtnBoard(bno, mno){ // 피신고자 커뮤니티 번호, 멤버번호 
+    const reportSubmitBtn = document.getElementById('report-submit-button');
+    reportSubmitBtn.setAttribute('onclick','reportBoard('+ bno +',' + mno + ')');
+}
+
+function changeSubmitBtnComment(cno, mno){ // 피신고자 댓글 번호, 멤버번호
+    const reportSubmitBtn = document.getElementById('report-submit-button');
+    reportSubmitBtn.setAttribute('onclick','reportComment('+ cno +',' + mno + ')');
+}
+
+function reportBoard(bno, mno){ // 피신고자 커뮤니티 번호, 멤버번호 
     const checkedButton = document.querySelector('#reportForm input[type="radio"][name="reportNumber"]:checked');
     const reportReason = document.querySelector("#reportForm textarea[name=reportReason]");
-    
+
     $.ajax({
         url : "report.bo",
         type: "post",
         data : {
-            communityNo : cno,
+            communityNo : bno,
+            reportedMemberNo : mno,
+            reportNumber : checkedButton.value,
+            reportReason : reportReason.value
+        },
+        success : function(res) {
+            if(res > 0) {
+                alert("성공적으로 신고되었습니다.");
+            } else {
+                alert("이미 신고한 글입니다.");
+            }
+        },
+        error : function() {
+            console.log("신고 AJAX 실패");
+        }
+    })
+}
+
+function reportComment(cno, mno){
+    const checkedButton = document.querySelector('#reportForm input[type="radio"][name="reportNumber"]:checked');
+    const reportReason = document.querySelector("#reportForm textarea[name=reportReason]");
+
+
+    $.ajax({
+        url : "report.co",
+        type: "post",
+        data : {
+            commentNo : cno,
             reportedMemberNo : mno,
             reportNumber : checkedButton.value,
             reportReason : reportReason.value
